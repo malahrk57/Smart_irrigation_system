eef6320983bba61986bf235c8bd08e7adf1e3e27
Rohit_Kumar <malahrk57@gmail.com>
2026-06-04 22:26:03 +0530

    update main.py

--- Python files/main.py
+++ Python files/main.py
@@ -4,24 +4,27 @@ from machine import Pin, ADC,SoftI2C
 import dht
 import BlynkLib
 from i2c_lcd import I2cLcd
-
+import urequests
 
 #I2C LCD setup
 i2c = SoftI2C(scl=Pin(22), sda = Pin(21))
 lcd = I2cLcd(i2c, 0x27, 2,16)
 
 # WiFi Credentials
-WIFI_SSID = "vivo T2x 5G"
+WIFI_SSID = "GalaxyM55"
 WIFI_PASS = "12345678"
 
 # Blynk Auth Token
-BLYNK_AUTH = "PiE7CYRuLPgVNKiVj2rtD7B9zGmTn_9n"
+BLYNK_AUTH = "PiE7CYRuLPgVNKiVj2BkD7B7yGmTn_9n"
 
 # Connect WiFi
 wifi = network.WLAN(network.STA_IF)
+wifi.active(False)
 wifi.active(True)
 wifi.connect(WIFI_SSID, WIFI_PASS)
 print("Connecting to WiFi...", end="")
+
+
 while not wifi.isconnected():
     print(".", end="")
     time.sleep(1)
@@ -54,13 +57,14 @@ while True:
         # control logic
         if rain_value == 1:
             rain_status = "No"
-            if soil_percent >= 69.50 :
+            if soil_percent >= 63.50 :
                 if temperature <= 29.9 or humidity >= 45.5 :
                     relay.value(0)
                     pump_status = "OFF"
                 else:
-                    relay.value(1)
-                    pump_status = "ON"
+                    if soil_percent >= 70:
+                        relay.value(0)
+                        pump_status = "OFF"
             else:
                 relay.value(1)
                 pump_status = "ON"
@@ -68,17 +72,7 @@ while True:
             rain_status = "Yes"
             relay.value(0)
             pump_status = "OFF"
-            
-            
-            
-        print("{},{},{},{},{}".format(
-            temperature,
-            humidity,
-            soil_percent,
-            rain_status,
-            pump_status
-        ))
-        
+
         # Send to Blynk
         blynk.virtual_write(0, soil_percent)
         blynk.virtual_write(1, temperature)
@@ -93,10 +87,25 @@ while True:
         lcd.move_to(0,1)
         lcd.putstr("S:{:.1f}% P:{}".format(soil_percent,pump_status))
         
+        
+        # Send data to spreadsheet
+        url = "https://script.google.com/macros/s/AKfycbwNKdx8CWydvWqR33dxl9PQ96BkUR86gc7Z40FbP3QmUMQKNMR3wLZnNAjcnZH772o91Q/exec?soil={}&temp={}&hum={}&rainfall={}&pumpstatus={}".format(
+    soil_percent,
+    temperature,
+    humidity,
+    rain_status,
+    pump_status
+)
+
+        response = urequests.get(url)
+        print(response.text)
+        response.close()
+        
     except Exception as e:
         print("Sensor error:",e)
 
-time.sleep(5)
+time.sleep(10)
+
 
 
 
